36aa3a3fa8006eb89877eb084b01f2de6adc3908
Gloria Villagran <glo.villagran@gmail.com>
2023-01-31 15:12:00 -0300

    consumiendo datos y mostrando en pantalla

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 import { example } from './data.js';
+import pokemon from './data/pokemon/pokemon.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+//console.log(data.pokemon);
+//console.log(data.pokemon[0]);
+//console.log(data.pokemon[3].name);
 
-console.log(example, data);
+const bloqueTarjetas = document.getElementById("bloqueTarjetas");
+bloqueTarjetas.innerHTML = ""
+for (const pokemon of data.pokemon){
+    bloqueTarjetas.innerHTML += `
+    <section>
+        <img src=${pokemon.img} alt="">
+        <p>${pokemon.name}</p>
+      </section>
+    `
+    console.log(pokemon.name);
+
+}
+
+//console.log(example, data);
