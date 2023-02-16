8ba74355be373e3acbc2037bd1b1a4e8b05e3159
YesicaChui <yesicachuic@gmail.com>
2023-02-15 22:57:05 -0500

    agregando texto alternativo a las imagenes

--- src/data.js
+++ src/data.js
@@ -8,7 +8,7 @@ export const tarjetaDataString = (data) => {
   for (const pokemon of data.pokemon) {
     tarjetaString += `
       <section class="tarjeta">
-        <img class="imagenPokemon" src=${pokemon.img} alt="">
+        <img class="imagenPokemon" src=${pokemon.img} alt=`${pokemon.img}`>
         <p>${pokemon.name}</p>
       </section>
     `
